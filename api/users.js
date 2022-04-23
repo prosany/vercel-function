7ab438cae06d659e8315fd695f436d93fde536af
Mahabub Sunny <mahabubsany2@gmail.com>
2022-04-23 12:17:18 +0600

    New Changes

--- api/users.js
+++ api/users.js
@@ -1,7 +1,5 @@
-const path = require("path");
-
 module.exports = (req, res) => {
   if (req.method === "GET") {
-    res.sendFile(path.resolve(path.join(__dirname, "../public/index.html")));
+    res.sendFile("../public/index.html");
   }
 };
